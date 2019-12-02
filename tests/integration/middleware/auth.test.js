d95a780ff4d90913b95154891e155c28a7eec3e7
Filip <filip.sokolowski1990@gmail.com>
2019-12-02 18:22:12 +0100

    Add test

--- tests/integration/middleware/auth.test.js
+++ tests/integration/middleware/auth.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const User = require('../../../models/users').Model;
+const Genre = require('../../../models/genres').Model;
 
 const endpoint = '/api/genres/';
 
@@ -15,16 +16,34 @@ describe('auth middleware', () => {
     });
 
     afterEach(async () => {
+        await Genre.deleteMany({});
         await server.close();
     });
 
     it.only('should return 401 if no token is provided', async () => {
-             const res = await request(server)
+        const res = await request(server)
             .post(endpoint)
             .set('x-auth-token', '')
             .send({ name: 'genre1' });
 
         expect(res.status).toBe(401);
+    });
 
-    })
+    it.only('should return 400 if token is invalid', async () => {
+        const res = await request(server)
+            .post(endpoint)
+            .set('x-auth-token', 'a')
+            .send({ name: 'genre1' });
+
+        expect(res.status).toBe(400);
+    });
+
+    it.only('should return 200 if token is valid', async () => {
+        const res = await request(server)
+            .post(endpoint)
+            .set('x-auth-token', token)
+            .send({ name: 'genre1' });
+
+        expect(res.status).toBe(200);
+    });
 });
